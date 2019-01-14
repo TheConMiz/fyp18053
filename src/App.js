4172f043e297a4d711621ed305ab54946a8807f1
ConMiz <ihandsomeguy97@gmail.com>
2019-01-14 16:18:06 +0800

    Fresh Installation of React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Portal from './components/Portal';
+
+class App extends React.Component {
+    render() {
+        return ( 
+            <div className = "App" >
+                <Portal></Portal> 
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Portal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Portal extends React.Component {
+    render() {
+        return ( 
+            <div className = "Portal" >
+                <h1> MAIN PORTAL </h1> 
+            </div>
+        );
+    }
+}
+
+export default Portal;
