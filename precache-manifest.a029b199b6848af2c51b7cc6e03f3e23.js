0fd2da7d2af56020fe03e440e6a33f450695d68b
ConMiz <ihandsomeguy97@gmail.com>
2019-04-08 14:31:42 +0800

    Updates

---
+++ precache-manifest.a029b199b6848af2c51b7cc6e03f3e23.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d09e76d8e5cbcb3981db",
+    "url": "/fyp18053/static/js/runtime~main.d09e76d8.js"
+  },
+  {
+    "revision": "9ad25e5e171e43d056e5",
+    "url": "/fyp18053/static/js/main.9ad25e5e.chunk.js"
+  },
+  {
+    "revision": "e09f280ebdf1aa42f01f",
+    "url": "/fyp18053/static/js/2.e09f280e.chunk.js"
+  },
+  {
+    "revision": "dd57212c44776ab85d6996d822f5ed1a",
+    "url": "/fyp18053/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fyp18053/precache-manifest.dd3346d87a94a889aa4e36ab3231d60f.js"
+  "/fyp18053/precache-manifest.a029b199b6848af2c51b7cc6e03f3e23.js"
 );
 
 workbox.clientsClaim();
